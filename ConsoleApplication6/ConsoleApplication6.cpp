5f017a92bda61ef2a23728e1631b42c15b43d623
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-03-23 16:49:47 +0300

    Update ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,35 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	setlocale(0, "ru");
-	int n1;
-	int n2;
-	int n3;
-	int counter1 = 0;
-
-	for (int i = 100; i < 1000; i++) 
+	setlocale(0, "RU");
+	int number, digit, i, newNumber;
+	cout << "Число: "<<endl;
+	cin >> number;
+	i = 0;
+	newNumber = 0;
+	while (number > 0)
 	{
-		n1 = i / 100;
-		n2 = i / 10 % 10;
-		n3 = i % 10;
+		digit = number % 10;
+		if ((digit != 3) && (digit != 6))
+		{
+			newNumber = newNumber + digit * pow(10, i);
+			i++;
+		}
 
-
-		if (n1 == n2 && n1!=n3 && n2!=n3) {
-			counter1++;
-		}
-		if (n2 == n1 && n2 != n3 && n1 != n3) {
-			counter1++;
-		}
-		if (n3 == n2 && n1 != n3 && n2 != n3) {
-			counter1++;
-		}
-		if (n1 == n3 && n1 != n2 && n3 != n2) {
-			counter1++;
-		}
+		number = number / 10;
 	}
-cout << "Количество целых чисел в диапазоне от 100 до 999, у которых есть две одинаковые цифры ->" << counter1;
-		
+	cout << "Новое число: " << newNumber;
 }
-
